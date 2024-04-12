076b4477a99b66d1e6838c58629c9fb5f16dd53c
sidTsui <147187017+sidTsui@users.noreply.github.com>
2024-04-12 15:22:56 -0400

    Delete src/main/java/com/example/csc331accountingapp/Marketing.java

--- src/main/java/com/example/csc331accountingapp/Marketing.java
+++
@@ -1,8 +0,0 @@
-package com.example.csc331accountingapp;
-
-public class Marketing extends Account{
-
-    public Marketing(String username, String password, int userID) throws Exception {
-        super(username, password, userID);
-    }
-}
