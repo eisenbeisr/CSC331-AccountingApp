6487652b9948456c42fcf14de60182f42eb90afa
sidTsui <147187017+sidTsui@users.noreply.github.com>
2024-04-12 15:23:29 -0400

    Delete src/main/java/com/example/csc331accountingapp/Production.java

--- src/main/java/com/example/csc331accountingapp/Production.java
+++
@@ -1,8 +0,0 @@
-package com.example.csc331accountingapp;
-
-public class Production extends Account{
-
-    public Production(String username, String password, int userID) throws Exception {
-        super(username, password, userID);
-    }
-}
