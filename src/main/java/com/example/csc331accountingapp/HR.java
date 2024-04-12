0ff0e6814cf02e1fc2a051ec083784b9c52af6ec
sidTsui <147187017+sidTsui@users.noreply.github.com>
2024-04-12 15:22:32 -0400

    Delete src/main/java/com/example/csc331accountingapp/HR.java

--- src/main/java/com/example/csc331accountingapp/HR.java
+++
@@ -1,8 +0,0 @@
-package com.example.csc331accountingapp;
-
-public class HR extends Account{
-
-    public HR(String username, String password, int userID) throws Exception {
-        super(username, password, userID);
-    }
-}
