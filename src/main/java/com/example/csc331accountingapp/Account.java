505af87153af7c45bf7f09f01046e94d7bdf7c43
eisenbeisr <163033179+eisenbeisr@users.noreply.github.com>
2024-04-21 15:22:37 -0400

    Updated Account.java

--- src/main/java/com/example/csc331accountingapp/Account.java
+++ src/main/java/com/example/csc331accountingapp/Account.java
@@ -1,14 +1,14 @@
-//sidney tsui
-//added: withdraw, deposit, and view balance
-//what's left: finish main class, set dept name
-
+/**
+ * Section: CSC331-001
+ * Group: 7
+ * Team: Ryan Eisenbeis, Jake Rouse, Oliver Rice, Sidney Tsui, Jackson Snyder
+ * Project Title: Accounting/Budgeting App
+ * Date: April 21, 2024.
+ * Purpose: User Account class
+ */
 
 package com.example.csc331accountingapp;
-// import java.util.Scanner;
 
-/**
- * user account
- */
 public class Account {
 
   //assign private instance variables
@@ -17,21 +17,24 @@ public class Account {
   private int userID;
 
   /**
-   * @param username
-   * @param password
-   * @param userID
-   * @throws Exception IllegalArgumentException if username or password is empty or null
+   * @param username: username entered at Login page
+   * @param password: password entered at Login page
+   * @param userID: userID to identify the user that is logging in
+   * @throws IllegalArgumentException IllegalArgumentException if username or password is empty or null
    */
   public Account(String username, String password, int userID) throws IllegalArgumentException {
+
     if ((username == null || username.isEmpty()) || (password == null || password.isEmpty())) {
       throw new IllegalArgumentException("invalid input");
     }
+
     this.username = username;
     this.password = password;
     this.userID = userID;
+
   }
 
-  //accessors
+  // Accessors
   public String getUsername() {
     return username;
   }
@@ -44,7 +47,7 @@ public class Account {
     return userID;
   }
 
-  //mutators
+  // Mutators
   public void setUsername(String username) {
     this.username = username;
   }
@@ -57,16 +60,17 @@ public class Account {
     this.userID = userID;
   }
 
-  public static class Department {
-    private String deptName;
-    private double budget;
-    private double balance;
+public class Department {
+  private String deptName;
+  private double budget;
+  private double balance;
 
     public Department(String deptName, double budget) {
       this.deptName = deptName;
       this.budget = budget;
     }
 
+    // Accessors
     public String getDeptName() {
       return deptName;
     }
@@ -79,23 +83,25 @@ public class Account {
       return balance;
     }
 
-    //question: are we using preset budgets for each dept, or having the user input it
-
-    /*public void setDeptName(){
+    // Mutators
+    public void setDeptName(){
       this.deptName = deptName;
     }
     public void setBudget(){
       this.budget = budget;
-    }*/
+    }
 
-    public void viewBalance(double balance) {
+  /**
+   * This method is used to view the current balance of a department
+   * @param balance: the department's balance
+   */
+  public void viewBalance(double balance) {
       System.out.println("Current balance of the " + deptName + " department: $" + balance);
     }
 
     /**
-     * allows user to deposit some amount of money then have their balance updated
-     *
-     * @param deposit_value
+     * This method allows user to deposit some amount of money then have their balance updated
+     * @param deposit_value: The amount being deposited into a department
      */
     public void deposit(double deposit_value) {
       if (deposit_value > 0) {
@@ -111,9 +117,8 @@ public class Account {
     }
 
     /**
-     * allows user to withdraw some amount of money then have their balance updated
-     *
-     * @param withdraw_value
+     * This method allows user to withdraw some amount of money then have their balance updated
+     * @param withdraw_value: The amount being withdrawn from a department
      */
     public void withdraw(double withdraw_value) {
       if (withdraw_value <= balance) {
@@ -132,4 +137,5 @@ public class Account {
     }
 
   }
+
 }
